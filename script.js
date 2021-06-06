6ece5c1b1dab6df6bc2e1df3fdc0fdc68a7a6520
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-06-06 04:51:48 +0100

    Add fetch to the DOM using getElementbyID and innerHTML

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 const APIURL = 'https://api.github.com/users/'
 
-const form = document.getElementById('form')
+const main = document.getElementById('main')
 const search = document.getElementById('search')
 
 
 async function getUser(username) {
     try {
         const {data} = await axios.get(APIURL + username)
-        console.log(data)
+        creatUserCard(data);   
     } catch(err) {
         console.log(err)
     }
     
         
 }
+
+
+function creatUserCard(user) {
+    const cardHTML = `
+                <div class="card">
+            <div class="picuser">
+                <img src="${user.avatar_url}" alt="${user.name}" class="">
+            </div>
+            <div class="userInfo">
+                <h2>
+                    ${user.name}
+                </h2>
+                    ${user.bio}
+                <ul>
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                    <li>${user.stargazers_count}<strong>Stars</strong></li>
+                </ul>
+
+                <div id="repos">
+                    
+                </div>
+            </div>
+        </div>
+    `
+    main.innerHTML = cardHTML;
+
+}
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
